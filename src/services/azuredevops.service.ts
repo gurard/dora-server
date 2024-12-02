d35dd9aab336de6f237b7865e48093d9a3588226
gwaycoder <drew.weber@mckesson.com>
2024-12-02 15:16:23 -0500

    determine timestamp of azure devops release

--- src/services/azuredevops.service.ts
+++ src/services/azuredevops.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { IDeployment } from '../models/deployment.interface';
 
 @Injectable()
 export class AzureDevOpsService {
@@ -19,9 +20,9 @@ export class AzureDevOpsService {
     pipeline: string,
     env: string,
   ) {
+    let deployments: IDeployment[] = [];
     const encodedOrg = encodeURIComponent(org);
     const encodedProject = encodeURIComponent(project);
-
     const url = `${this.platformUrl}/${encodedOrg}/${encodedProject}/_apis/release/releases?api-version=7.1&$expand=environments`;
 
     try {
@@ -33,16 +34,31 @@ export class AzureDevOpsService {
         (release) => release.releaseDefinition.name === pipeline,
       );
 
-      // Filter for specific environment
-      filteredReleases = filteredReleases.filter((release) =>
-        release.environments.some(
+      // filter for specific environment
+      for (let i = 0; i < filteredReleases.length; ++i) {
+        let release = filteredReleases[i];
+
+        // find env of the release and make sure it has succeeded (filter out pending)
+        const releaseEnv = release.environments.find(
           (environment) =>
             environment.name.toLowerCase() === env.toLowerCase() &&
             environment.status === 'succeeded',
-        ),
-      );
+        );
 
-      return filteredReleases;
+        if (releaseEnv) {
+          // deployment time is determined by timestamp of final step in deployment
+          if (releaseEnv.deploySteps && releaseEnv.deploySteps.length > 0) {
+            const lastStep =
+              releaseEnv.deploySteps[releaseEnv.deploySteps.length - 1];
+            if (lastStep && lastStep.lastModifiedOn) {
+              deployments.push({ deploymentDate: lastStep.lastModifiedOn });
+            }
+          }
+        }
+      }
+
+      // return deploymentsWithCompletionTime;
+      return deployments;
     } catch (error) {
       console.error('Error fetching deployments:', error);
       throw new Error('Failed to fetch deployments');
