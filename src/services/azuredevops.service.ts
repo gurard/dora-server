200ff2a30154b2e1067b38f07d983934e12032f1
gwaycoder <drew.weber@mckesson.com>
2024-12-02 13:59:11 -0500

    filter releases that have completed for a specific environment

--- src/services/azuredevops.service.ts
+++ src/services/azuredevops.service.ts
@@ -22,12 +22,27 @@ export class AzureDevOpsService {
     const encodedOrg = encodeURIComponent(org);
     const encodedProject = encodeURIComponent(project);
 
-    const url = `${this.platformUrl}/${encodedOrg}/${encodedProject}/_apis/release/releases?api-version=7.1`;
+    const url = `${this.platformUrl}/${encodedOrg}/${encodedProject}/_apis/release/releases?api-version=7.1&$expand=environments`;
 
-    console.log('url', url);
     try {
       const response = await axios.get(url, { headers: this.getHeaders() });
-      return response.data;
+      const releases = response.data.value;
+
+      // Filter for specific pipeline
+      let filteredReleases = releases.filter(
+        (release) => release.releaseDefinition.name === pipeline,
+      );
+
+      // Filter for specific environment
+      filteredReleases = filteredReleases.filter((release) =>
+        release.environments.some(
+          (environment) =>
+            environment.name.toLowerCase() === env.toLowerCase() &&
+            environment.status === 'succeeded',
+        ),
+      );
+
+      return filteredReleases;
     } catch (error) {
       console.error('Error fetching deployments:', error);
       throw new Error('Failed to fetch deployments');
