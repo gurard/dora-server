b5fb83ec4abad0b18dc8c40aee9f876b535a5a3a
gwaycoder <drew.weber@mckesson.com>
2024-12-05 16:56:23 -0500

    b/g params to all be query params, remove body params

--- src/controllers/kubernetes.controller.ts
+++ src/controllers/kubernetes.controller.ts
@@ -10,23 +10,27 @@ export class KubernetesController {
   async getServiceBlueGreen(
     @Query('namespace') namespace: string,
     @Query('service') service: string,
-    @Body('blueLabel') blueLabel: string,
-    @Body('greenLabel') greenLabel: string,
+    @Query('blueLabel') blueLabel: string,
+    @Query('greenLabel') greenLabel: string,
   ) {
     let response: IService = { service: service, blue: false, green: false };
 
-    let labels = await this.kubernetesService.getServiceLabels(
-      namespace,
-      service,
-    );
+    try {
+      let labels = await this.kubernetesService.getServiceLabels(
+        namespace,
+        service,
+      );
 
-    let appLabel = labels['app'];
+      let appLabel = labels['app'];
 
-    if (appLabel) {
-      response.blue = appLabel === blueLabel;
-      response.green = appLabel === greenLabel;
+      if (appLabel) {
+        response.blue = appLabel === blueLabel;
+        response.green = appLabel === greenLabel;
+      }
+
+      return response;
+    } catch (error) {
+      return error;
     }
-
-    return response;
   }
 }
